f8eeb82eb0ec8369181f5b249bf713f6e0c9dc25
Daniel Weidner <hallo@danielweidner.de>
2021-02-10 08:32:48 +0100

    chore: generate new build

--- dist/markdown-it-attribution.js
+++ dist/markdown-it-attribution.js
@@ -350,6 +350,9 @@ module.exports = function attributionPlugin (md, options) {
 
       // Skip the generated block quote tokens in the stream.
       i = end + 5;
+
+      // Update the length of the token stream.
+      l = l + 4;
     }
   }
 
